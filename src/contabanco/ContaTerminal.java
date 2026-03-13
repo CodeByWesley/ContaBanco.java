a178064fa3bef601f743749341eee0cdd26a3d9a
CodeByWesley <wesleyopip@gmail.com>
2026-03-12 21:23:18 -0300

    Add Sapphire Bank welcome message

--- src/contabanco/ContaTerminal.java
+++ src/contabanco/ContaTerminal.java
@@ -2,6 +2,8 @@ package contabanco;
 
 public class ContaTerminal {
     public static void main (String [] args){
-        System.out.println("oi");
+        System.out.println("=================================");
+        System.out.println("\"Welcome to Sapphire Bank!\"");
+        System.out.println("=================================");
     }
 }
