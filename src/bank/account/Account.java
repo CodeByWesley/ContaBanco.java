ff32a7d060ef3165cce3a1c1d372e8b1100ad8f9
CodeByWesley <wesleyopip@gmail.com>
2026-03-14 14:20:54 -0300

    adding deposit and withdrawal functions

--- src/bank/account/Account.java
+++ src/bank/account/Account.java
@@ -2,11 +2,23 @@ package bank.account;
 
 public class Account {
    private String agency;
-   private String conta;
+   private String accountNumber;
    private String clientName;
    private String taxIdNumber;
    private String email;
    private String password;
    private Address address;
+
    private double balance;
+
+   public void deposit(double value) {
+      balance += value;
+   }
+   public void withdraw(double value) {
+      if (value > balance) {
+         System.out.println("Insufficient balance.");
+         return;
+      }
+      balance -= value;
+   }
 }
